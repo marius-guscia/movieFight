f43f12c009bc1c69b402aa3a5dfb4a3e4fe18ef8
Marius Guscia <marius.guscia@gmail.com>
2021-05-14 14:48:22 +0300

    Added search input & API fetch logic with input debounce.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hi there!');
+// fetchData function using Axios
+
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'af88c15',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data)
+};
+
+// Selecting input field
+
+const input = document.querySelector('input');
+
+// Delayed search inquiry / DEBOUNCING AN INPUT
+
+let timeoutId;
+
+const onInput = event => {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+        fetchData(event.target.value);
+    }, 1000);
+};
+
+// Listening for input changes.
+
+input.addEventListener('input', onInput);
