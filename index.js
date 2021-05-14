9e107470cad0730320bb49f0f38ee0a3f49c2908
Marius Guscia <marius.guscia@gmail.com>
2021-05-14 16:33:22 +0300

    Added dropdown & movie search logic.

--- index.js
+++ index.js
@@ -7,34 +7,60 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
-
-    console.log(response.data)
+    if (response.data.Error) {
+        return [];
+    }
+    return response.data.Search;
 };
 
 // Selecting input field
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+<label><b>Search For a Movie:</b></label>
+<input class="input"/>
+<div class="dropdown container">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>
+`;
+
 const input = document.querySelector('input');
-
-// Delayed search inquiry function / DEBOUNCING AN INPUT
-
-const debounce = (callback, delay = 1000) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            callback.apply(null, args);
-        }, delay);
-    };
-};
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 // Fetching data on user input
 
-const onInput = event => {
-    fetchData(event.target.value);
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    if (!movies.length) {
+        dropdown.classList.remove('is-active')
+        return;
+    }
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+        const option = document.createElement('a');
+        const imgSrc = (movie.Poster === 'N/A' ? '' : movie.Poster);
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
+        <img src=${imgSrc} />
+        ${movie.Title}
+        `;
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+        });
+        resultsWrapper.appendChild(option);
+    }
 };
 
 // Listening for input changes.
 
-input.addEventListener('input', debounce(onInput, 500));
+input.addEventListener('input', debounce(onInput, 1000));
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+
+    }
+});
---
+++ utils.js
@@ -0,0 +1,13 @@
+// Delayed search inquiry function / DEBOUNCING AN INPUT
+
+const debounce = (callback, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            callback.apply(null, args);
+        }, delay);
+    };
+};
