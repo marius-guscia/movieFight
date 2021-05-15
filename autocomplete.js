c1a87cbc739be44afc7950e7806b5349840232bf
Marius Guscia <marius.guscia@gmail.com>
2021-05-15 10:43:16 +0300

    Moved search autocomplete logic to a separate file.

---
+++ autocomplete.js
@@ -0,0 +1,49 @@
+const createAutoComplete = ({ root, renderOption, onOptionSelect, inputValue, fetchData }) => {
+    root.innerHTML = `
+    <label><b>Search:</b></label>
+    <input class="input"/>
+    <div class="dropdown container">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+    `;
+
+    // Selecting input field
+
+    const input = root.querySelector('input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWrapper = root.querySelector('.results');
+
+    // Fetching data on user input
+
+    const onInput = async event => {
+        const items = await fetchData(event.target.value);
+        if (!items.length) {
+            dropdown.classList.remove('is-active')
+            return;
+        }
+        resultsWrapper.innerHTML = '';
+        dropdown.classList.add('is-active');
+        for (let item of items) {
+            const option = document.createElement('a');
+
+            option.classList.add('dropdown-item')
+            option.innerHTML = renderOption(item);
+            option.addEventListener('click', () => {
+                dropdown.classList.remove('is-active');
+                input.value = inputValue(item);
+                onOptionSelect(item);
+            });
+            resultsWrapper.appendChild(option);
+        }
+    };
+
+    // Listening for input changes.
+    input.addEventListener('input', debounce(onInput, 1000));
+    document.addEventListener('click', event => {
+        if (!root.contains(event.target)) {
+
+        }
+    });
+};
--- index.js
+++ index.js
@@ -1,119 +1,80 @@
 // fetchData function with limited data using Axios
-
-const fetchData = async (searchTerm) => {
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+  renderOption(movie) {
+    const imgSrc = (movie.Poster === 'N/A' ? '' : movie.Poster);
+    return `
+    <img src=${imgSrc} />
+    ${movie.Title} (${movie.Year})
+    `
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
     const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: 'af88c15',
-            s: searchTerm
-        }
+      params: {
+        apikey: 'af88c15',
+        s: searchTerm
+      }
     });
     if (response.data.Error) {
-        return [];
+      return [];
     }
     return response.data.Search;
-};
+  }
+});
 
 // fetchData function with extra data using Axios
 
 const onMovieSelect = async (movie) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: 'af88c15',
-            i: movie.imdbID
-        }
-    });
-    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'af88c15',
+      i: movie.imdbID
+    }
+  });
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
 }
 
 const movieTemplate = (movieDetail) => {
-    return `
-    <article class="media">
-    <figure class="media-left">
-      <p class="image">
-        <img src=${movieDetail.Poster} />
-      </p>
-    </figure>
-    <div class="media-content">
-      <div class="content">
-        <h1>${movieDetail.Title}</h1>
-        <h4>${movieDetail.Genre}</h4>
-        <p>${movieDetail.Plot}</p>
-      </div>
+  return `
+  <article class="media">
+  <figure class="media-left">
+    <p class="image">
+      <img src=${movieDetail.Poster} />
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <h1>${movieDetail.Title}</h1>
+      <h4>${movieDetail.Genre}</h4>
+      <p>${movieDetail.Plot}</p>
     </div>
-    </article>
-    <article class="notification is-primary">
-    <p class="title">${movieDetail.Awards}</p>
-    <p class="subtitle">Awards</p>
+  </div>
   </article>
   <article class="notification is-primary">
-    <p class="title">${movieDetail.BoxOffice}</p>
-    <p class="subtitle">BoxOffice</p>
-  </article>
-  <article class="notification is-primary">
-    <p class="title">${movieDetail.Metascore}</p>
-    <p class="subtitle">Metascore</p>
-  </article>
-  <article class="notification is-primary">
+  <p class="title">${movieDetail.Awards}</p>
+  <p class="subtitle">Awards</p>
+</article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.BoxOffice}</p>
+  <p class="subtitle">BoxOffice</p>
+ </article>
+<article class="notification is-primary">
+  <p class="title">${movieDetail.Metascore}</p>
+  <p class="subtitle">Metascore</p>
+</article>
+<article class="notification is-primary">
     <p class="title">${movieDetail.imdbRating}</p>
     <p class="subtitle">IMDB Rating</p>
-  </article>
-  <article class="notification is-primary">
+</article>
+<article class="notification is-primary">
     <p class="title">${movieDetail.imdbVotes}</p>
     <p class="subtitle">IMDB Votes</p>
-  </article>
-    `;
-}
-
-// Selecting input field
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-<label><b>Search For a Movie:</b></label>
-<input class="input"/>
-<div class="dropdown container">
-    <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-    </div>
-</div>
+</article>
 `;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-// Fetching data on user input
-
-const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-    if (!movies.length) {
-        dropdown.classList.remove('is-active')
-        return;
-    }
-    resultsWrapper.innerHTML = '';
-    dropdown.classList.add('is-active');
-    for (let movie of movies) {
-        const option = document.createElement('a');
-        const imgSrc = (movie.Poster === 'N/A' ? '' : movie.Poster);
-        option.classList.add('dropdown-item')
-        option.innerHTML = `
-        <img src=${imgSrc} />
-        ${movie.Title}
-        `;
-        option.addEventListener('click', () => {
-            dropdown.classList.remove('is-active');
-            input.value = movie.Title;
-            onMovieSelect(movie);
-        });
-        resultsWrapper.appendChild(option);
-    }
-};
-
-// Listening for input changes.
-
-input.addEventListener('input', debounce(onInput, 1000));
-document.addEventListener('click', event => {
-    if (!root.contains(event.target)) {
-
-    }
-});
-
+}
